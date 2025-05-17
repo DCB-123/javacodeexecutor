041b8ec1a724bedc737a54a9b9250dc17d3c54c9
DCB <bhattdeepak1998@gmail.com>
2025-05-17 14:04:38 +0530

    Allow CORS for Netlify frontend URL

--- src/main/java/com/codeeditor/javacodeexecutor/config/WebConfig.java
+++ src/main/java/com/codeeditor/javacodeexecutor/config/WebConfig.java
@@ -14,7 +14,10 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3001")
+                        .allowedOrigins(
+                                "http://localhost:3001",
+                                "https://elaborate-moonbeam-c9d7d1.netlify.app"
+                        )
                         .allowedMethods("*");
             }
         };
